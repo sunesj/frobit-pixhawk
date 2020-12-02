0f8b9bfe4f12af68b2d6df4d2c148996905d46a7
Sune Straarup Jensen <sune@nordbo-robotics.com>
2020-12-02 11:09:29 +0100

    Modified setup to avoid reboot

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,19 +16,37 @@ sudo usermod -aG docker "$USER"
 newgrp docker
 
 echo "Cloning code from GitHub"
-src_dir=$HOME/Frobit-Pixhawk
-git clone https://github.com/sunesj/frobit-pixhawk.git "$src_dir"
-cd "$src_dir" || (echo "Unable to enter directory $src_dir. Aborting setup..." && exit)
+frobit_dir=$HOME/Frobit-Pixhawk
+git clone https://github.com/sunesj/frobit-pixhawk.git "$frobit_dir"
 
 echo "Configuring crontabs"
+current_dir=$PWD
+cd "$frobit_dir" || (echo "Unable to enter directory $frobit_dir. Aborting setup..." && exit)
 temp_file=tmp_crontab_file
+cmd_signal_alive=$frobit_dir/scripts/signal_alive.sh
+cmd_check_status="/usr/bin/python3 $frobit_dir/scripts/check_status.py"
 crontab -l > $temp_file
-echo "@reboot $src_dir/scripts/signal_alive.sh" >> $temp_file
+echo "@reboot $cmd_signal_alive" >> $temp_file
 crontab $temp_file
 # shellcheck disable=SC2024
 sudo crontab -l > $temp_file
-echo "@reboot /usr/bin/python3 $src_dir/scripts/check_status.py" >> $temp_file
+echo "@reboot @cmd_check_status" >> $temp_file
 sudo crontab $temp_file
 rm $temp_file
+# shellcheck disable=SC2164
+cd "$current_dir"
 
-echo "Done with setup. Please reboot..."
+# Starts script to avoid a reboot
+echo "Starting status scripts"
+$cmd_signal_alive &
+$cmd_check_status &
+
+echo ""
+echo "Setup done..."
+echo ""
+echo "Start Frobit by running these commands:"
+echo ""
+echo "  cd $frobit_dir"
+echo "  docker-compose up"
+
+
