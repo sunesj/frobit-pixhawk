e5c2e87d1b81e4d86470f0b9deb0b9fc7f25cc03
Sune Straarup Jensen <sunesj@outlook.com>
2020-12-22 14:55:32 +0100

    Reverted log rate change

--- ros-px4-adapter/scripts/px4_adapter.py
+++ ros-px4-adapter/scripts/px4_adapter.py
@@ -35,15 +35,10 @@ class PX4Adapter():
 
     def spin(self):
         r = rospy.Rate(1)
-        count = 0
         while not rospy.is_shutdown():
-            if count == 10:
-                count = 0
-                if self.has_published:
-                    rospy.loginfo("Alive - Has published")
-                    self.has_published = False
-                else:
-                    rospy.loginfo("Alive - Nothing published")
-            count = count + 1
+            if self.has_published:
+                rospy.loginfo("Alive - Has published")
+                self.has_published = False
+            else:
+                rospy.loginfo("Alive - Nothing published")
             r.sleep()
-
