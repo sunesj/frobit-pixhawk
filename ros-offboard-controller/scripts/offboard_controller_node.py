eda3c471d97a7e7e6d6fe2f53b87874f31b2812e
Sune Straarup Jensen <sunesj@outlook.com>
2020-12-22 14:29:50 +0100

    Added log messages

--- ros-offboard-controller/scripts/offboard_controller_node.py
+++ ros-offboard-controller/scripts/offboard_controller_node.py
@@ -21,14 +21,26 @@ try:
     signal = WaitingClass()
 
     while not rospy.is_shutdown():
+
+        rospy.loginfo('waiting for remote control ready signal')
         while not signal.ready:
             time.sleep(0.5)
+        rospy.loginfo('ready to load and run offboard mission')
 
         node.load_mission("coordinates_1.csv")
+        rospy.loginfo('Mission loaded')
+
         node.arm()
+        rospy.loginfo('Vehicle armed')
+
         node.set_offboard_mode()
+        rospy.loginfo('Offboard mode set')
+
         node.mission()
+        rospy.loginfo('Mission complete')
+
         node.disarm()
+        rospy.loginfo('disarmed')
 
         signal.ready = False
 
